a3131633b533c39d3ad6a88e878c8d3bd7ec8dd8
shimizuyuta <shimizuyuta213@gmail.com>
2025-10-19 17:08:39 +0900

    delete: for api server basic

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,5 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// API routes will be defined in routes/api.php
+// This file is kept for any web-specific routes if needed in the future
